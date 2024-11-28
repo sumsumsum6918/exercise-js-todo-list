3c8e6e3bf44d2c9091d22c5e420988da22cbe649
sum <tinafongfong@gmail.com>
2024-11-28 17:22:33 +0100

    generate basic html with js and generate task id with uuid

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,10 @@ function addNewTask() {
   const dateTimeElement = document.getElementById("task-date").value;
   const initialsInputElement = document.getElementById("task-author").value;
 
+  const newUUID = uuid.v4();
+
   const newTask = {
+    id: newUUID,
     name: titleInputElement,
     description: descriptionInputElement,
     dueDate: dateTimeElement,
@@ -26,4 +29,37 @@ newTaskForm.addEventListener("submit", (event) => {
   addNewTask();
 });
 
-function render() {}
+renderInProgressGrid();
+
+function renderInProgressGrid() {
+  let inProgressHTML = "";
+
+  todoList.forEach((task) => {
+    inProgressHTML += `
+      <div class="todo-container">
+          <div class="todo-header">
+            <h3 class="todo-title">Feed Pokemon</h3>
+            <div class="todo-actions">
+              <span class="size-18 material-symbols-outlined"> edit </span>
+              <span class="size-18 material-symbols-outlined"> delete </span>
+              <span class="size-18 material-symbols-outlined">
+                drag_indicator
+              </span>
+            </div>
+          </div>
+          <p class="todo-date">Saturday, 02 December 10:00</p>
+          <p class="todo-description">Make breakfast</p>
+          <div class="todo-footer">
+            <div class="author-container">
+              <div class="author">SF</div>
+            </div>
+            <span class="done-marker material-symbols-outlined">
+              done_all
+            </span>
+          </div>
+        </div>
+    `;
+  });
+
+  document.querySelector(".js-in-progress-grid").innerHTML = inProgressHTML;
+}
--- scripts/list.js
+++ scripts/list.js
@@ -1,17 +1,20 @@
 export const todoList = JSON.parse(localStorage.getItem("todoList")) || [
   {
+    id: "702cfb8f-fb84-4605-91c4-4f4746e1ce6e",
     title: "make dinner",
     description: "chicken",
     dueDate: "2024-11-26T11:00",
     author: "SI",
   },
   {
+    id: "542aca79-85f7-4f49-8984-1b853c5acaaf",
     name: "do dishes",
     description: "empty it first",
     dueDate: "2024-11-26T11:00",
     author: "SI",
   },
   {
+    id: "d971e0be-f071-486c-9271-23bdff6dc7dd",
     name: "open christmas calender",
     description: "sweets and scratch card",
     dueDate: "2024-12-01T11:00",
